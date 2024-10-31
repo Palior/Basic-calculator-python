cdf850bd5c714d9928e63f6b9d1b43145d7e6a5d
Palior <plizama.orostica@gmail.com>
2024-10-31 12:41:44 -0300

    Update calculadora.py
    Calculator updated to english version

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,32 @@
-def suma(a,b):
-    return (a+b)
-def resta(a,b):
-    return (a-b)
-def multiplicacion(a,b):
-    return (a*b)
-def division(a,b):
-    return (a/b)
-print ("Favor ingresar operacion a realizar")
-print("1 - SUMA")
-print("2 - RESTA")
-print("3 - MULTIPLICACION")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("Please enter the operation to perform:")
+print("1 - ADDITION")
+print("2 - SUBTRACTION")
+print("3 - MULTIPLICATION")
 print("4 - DIVISION")
-operacion = input("\n")
-a=int(input("ingrese el primer numero:"))
-b=int(input("ingrese el segundo numero:"))
-if (operacion=="1"):
- print("El resultado es : ", suma(a,b))
-elif (operacion=="2"):
- print("El resultado es : ", resta(a,b))
-elif (operacion=="3"):
- print("El resultado es : ", multiplicacion(a,b))
-elif (operacion=="4"):
- print("El resultado es : ", division(a,b))
+
+operation = input("\n")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+if operation == "1":
+    print("The result is:", add(a, b))
+elif operation == "2":
+    print("The result is:", subtract(a, b))
+elif operation == "3":
+    print("The result is:", multiply(a, b))
+elif operation == "4":
+    print("The result is:", divide(a, b))
 else:
- print("Ingrese una operacion valida")
+    print("Please enter a valid operation")
